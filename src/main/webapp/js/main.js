8a23a0b8b811a165e94919307973afc8d829da9c
Manal Afzal <manal.afzal@ontariotechu.net>
2024-04-17 19:26:02 -0400

    Completed scrambled word game functionality

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -236,7 +236,7 @@ document.getElementById('playTicTacToe').addEventListener('click', function() {
 // event listener for calling Play WordGame on  button click
 document.getElementById('playWordGame').addEventListener('click', function() {
     if (inRoom) {
-        window.location.href = 'guess.html';
+        window.location.href = 'wordGame.html';
     } else {
         alert('Please join a room to play Word Game!');
     }
